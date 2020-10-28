14ea8534a686a63e48838d8bcd72093ebf108b8f
n8creator <n8creator@pm.me>
2020-10-28 15:47:54 +0300

    Step 4: Tests added & .gitignore fixed

---
+++ tests/test.py
@@ -0,0 +1,10 @@
+from os import read
+from gendiff.generate_diff import generate_diff
+
+
+def test_diff_json():
+    """Simple test for flat JSON files."""
+    check = open('tests/fixtures/result.txt', 'r', encoding='UTF-8')
+    assert generate_diff('tests/fixtures/file1.json',
+                         'tests/fixtures/file2.json') == check.read()
+    check.close()
