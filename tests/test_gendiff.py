9417a1c608b261acfdc0b7b0397711a47a81166c
n8creator <n8creator@pm.me>
2020-11-02 16:43:32 +0300

    Step 5: json files for testing renamed

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,7 +3,7 @@ from gendiff.generate_diff import generate_diff
 
 def test_diff_json():
     """Simple test for flat JSON files."""
-    check = open('tests/fixtures/result.txt', 'r', encoding='UTF-8')
-    assert generate_diff('tests/fixtures/file1.json',
-                         'tests/fixtures/file2.json') == check.read()
+    check = open('tests/fixtures/diff_json.txt', 'r', encoding='UTF-8')
+    assert generate_diff('tests/fixtures/input.json',
+                         'tests/fixtures/output.json') == check.read()
     check.close()
